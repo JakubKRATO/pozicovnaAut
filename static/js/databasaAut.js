0c363fd93822b84ee161e919f311ce559030e11c
Jakub Kratochvíl <kubko.kratochvil@gmail.com>
2025-05-15 14:01:35 +0200

    vozidla gui added

---
+++ static/js/databasaAut.js
@@ -0,0 +1,14 @@
+const auta = [
+    {
+        id: 1,
+        displayName: "Arteon SB",
+        debugName: "arteonsb",
+        limitkm: 150,
+        cenakmNavyse: 0.1,
+        jednaTri: 70,
+        styriDesat: 60,
+        jedenastDvadsat: 50,
+        dvadsatjednaTridsat: 40,
+        zaloha: 500,
+    },
+]
