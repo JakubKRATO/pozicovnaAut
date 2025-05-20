d1c4205759c7fc7dc6db42406a3900f7c790e847
Jakub Kratochvíl <kubko.kratochvil@gmail.com>
2025-05-20 11:11:13 +0200

    potvrdenie

--- static/js/potvrdenie.js
+++ static/js/potvrdenie.js
@@ -1,13 +1,84 @@
 import auta from "./databasaAut.js"
 
-const id = sessionStorage.getItem("id")
-console.log(id);
+const id = parseInt(sessionStorage.getItem("id"))
 if (!id) {
     window.location.href = "/vozidla"
 }
+const auto = auta.find(a => a.id === id)
+var pismeno
 
+if (auto.pocetMiest == 4) {
+    pismeno = "a"
+} else {
+    pismeno = ""
+}
+const m1 = sessionStorage.getItem("m1") || null
+const m2 = sessionStorage.getItem("m2") || null
+const d1 = sessionStorage.getItem("d1") || null
+const d2 = sessionStorage.getItem("d2") || null
+const c1 = sessionStorage.getItem("c1") || null
+const c2 = sessionStorage.getItem("c2") || null
+console.log(c1,c2);
+
+const m1Input = document.getElementById("m1")
+const m2Input = document.getElementById("m2")
+const d1Input = document.getElementById("d1")
+const d2Input = document.getElementById("d2")
+const t1Input = document.getElementById("c1")
+const t2Input = document.getElementById("c2")
 
 const obr1 = document.getElementsByClassName("car-image")[0]
-const carName = document.getElementById("car-name")
+const carInfo = document.getElementsByClassName("car-info")[0]
 
-carName.innerHTML = auto.displayName
+const coreFun = () => {
+    try {
+        fillDataIntoForm()
+        renderInfo()
+        calculatePrice()
+        console.log("success");
+    } catch (error) {
+        console.log(error);
+    }
+}
+const fillDataIntoForm = () => {
+    let data = [m1, m2, d1, d2, c1, c2];
+    let array = [m1Input,m2Input,d1Input,d2Input,t1Input,t2Input]
+    for (let i = 0;i < 6;i++) {
+        if (data[i] == null || data[i].length == 0) {
+            continue;
+        }
+        array[i].value = data[i]
+    }
+};
+const renderInfo = () => {
+    carInfo.innerHTML = `<h2 id="car-name"> ${auto.displayName}</h2>
+                        <p class="car-specs">
+                        <span><i class="fas fa-cog"></i> ${auto.prevodovka}</span>
+                        <span class="separator">•</span>
+                        <span><i class="fas fa-gas-pump"></i> ${auto.palivo}</span>
+                        <span class="separator">•</span>
+                        <span><i class="fas fa-users"></i> ${auto.pocetMiest} miest${pismeno}</span>
+                        <span class="separator">•</span>
+                        <span><i class="fas fa-car"></i> ${auto.kategorie}</span>
+                        </p>
+                        <div class="car-image">
+                        </div>`
+    document.getElementsByClassName("car-image")[0].style.backgroundImage = `url(/img/auta/${auto.debugName}/1.png)`
+};
+const calculatePrice = () => {
+    const start = new Date(`${d1}T${c1}`);
+    const end   = new Date(`${d2}T${c2}`);
+
+    if (end < start) {
+      throw new Error("End date/time must be after start date/time");
+    }
+
+    const msPerDay = 1000 * 60 * 60 * 24;
+    const diffMs   = end - start;
+
+    const days = Math.max(1, Math.ceil(diffMs / msPerDay));
+
+    console.log(days);
+};
+
+coreFun()
--- static/js/rezervePageRender.js
+++ static/js/rezervePageRender.js
@@ -16,15 +16,21 @@ if (auto.pocetMiest == 4) {
 var m1 = null
 var m2 = null
 
+var d1 = null
+var d2 = null
+
 var c1 = null
 var c2 = null
 
-var t1 = null
-var t2 = null
-
+const submitButton = document.getElementById("continue")
 const cartitle = document.getElementsByClassName("car-title")[0];
 const obrazky = document.getElementsByClassName("obr")
-const customInputs = document.getElementsByClassName("not-necessary")
+const customInputs = document.getElementsByClassName("custom")
+
+const d1Input = document.getElementById("pickup-date")
+const d2Input = document.getElementById("return-date")
+const t1Input = document.getElementById("pickup-time")
+const t2Input = document.getElementById("return-time")
 
 document.getElementById("prevodovka").innerHTML = `<span id="prevodovka"><i class="fas fa-cog"></i> ${auto.prevodovka}</span>`
 document.getElementById("palivo").innerHTML = `<span id="palivo"><i class="fas fa-gas-pump"></i> ${auto.palivo}</span>`
@@ -39,9 +45,77 @@ for (let i = 0; i < obrazky.length; i++) {
 
 document.getElementById("pickup-location").addEventListener("change",(event) => {
   m1 = event.target.value
-  if (m1 === "ine") {
-    if (customInputs[0].classList.contains("not-necessary")) {
-      customInputs[0].classList.remove("not-necessary")
-    }
+  if (m1 !== "ine" && m2 !== "ine") {
+    customLogic(true,1,false)
+    customLogic(true,2,false)
+    customLogic(false,0,false)
+    return;
   }
+  if (m1 === "ine") {
+    customLogic(true,1,true)
+    m1 = null
+  } else {
+    customLogic(true,1,false)
+  }
+});
+document.getElementById("return-location").addEventListener("change",(event) => {
+  m2 = event.target.value
+  if (m1 !== "ine" && m2 !== "ine") {
+    customLogic(true,1,false)
+    customLogic(true,2,false)
+    customLogic(false,0,false)
+    return;
+  }
+  if (m2 === "ine") {
+    customLogic(true,2,true)
+    m2 = null
+  } else {
+    customLogic(true,2,false)
+  }
+});
+
+d1Input.addEventListener("change",(event) => {
+  d1 = d1Input.value
+})
+d2Input.addEventListener("change",(event) => {
+  d2 = d2Input.value
+})
+t1Input.addEventListener("change",(event) => {
+  c1 = t1Input.value
+})
+t2Input.addEventListener("change",(event) => {
+  c2 = t2Input.value
+})
+customInputs[0].addEventListener("input",() => {
+  m1 = customInputs[0].value
+});
+customInputs[1].addEventListener("input",() => {
+  m2 = customInputs[1].value
+});
+
+const customLogic = (display,ktore,status) => {
+  if (display) {
+    customInputs[0].style.display = "initial"
+    customInputs[1].style.display = "initial"
+    if (ktore == 1) {
+      customInputs[0].style.visibility = status ? "visible" : "hidden"
+    }
+    if (ktore == 2) {
+      customInputs[1].style.visibility = status ? "visible" : "hidden"
+    }
+  } else if (!display) {
+    customInputs[0].style.display = "none"
+    customInputs[1].style.display = "none"
+  }
+};
+
+submitButton.addEventListener("click",() => {
+  m1 = !!m1 ? m1 : document.getElementById("customVyzdvihnutie").value
+  m2 = !!m2 ? m2 : document.getElementById("customOdovzdanie").value
+  let data = [m1, m2, d1, d2, c1, c2];
+  let type = ["m1", "m2", "d1", "d2", "c1", "c2"];
+  for (let i in data) {
+    sessionStorage.setItem(type[i],data[i])
+  }
+  window.location.href = "/potvrdenie"
 });
