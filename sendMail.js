eff5598ede18e9bc1e99ccf18ed2d59d835f59c9
Jakub Kratochvíl <kubko.kratochvil@gmail.com>
2025-06-11 11:08:30 +0200

    eamil image display works now

--- sendMail.js
+++ sendMail.js
@@ -133,56 +133,51 @@ const sendMailClient = (data) => {
         to: data.email,
         subject: "Doklad o vašej objednávke",
         html: `<div style="font-family: Arial, sans-serif; max-width: 700px; margin: auto; background-color: #1a1a1a; color: #ffffff; padding: 30px; border-radius: 12px; border: 1px solid #333;">
-                <div style="
-                    background-image: url(https://raw.githubusercontent.com/JakubKRATO/pozicovnaAut/main/static/img/auta/${data.auto.debugName}/1.webp));
-                    background-size: cover; 
-                    background-position: center;
-                    min-width: 300px;
-                    min-height: 169px;
-                    max-width: 300px;
-                    max-height: 169px;
-                    aspect-ratio: 16/9;
-                    border-radius: 5px;
-                    margin-top: 20px;"></div>
-                <h2>${data.auto.displayName}</h2>
-                    <h3 style="margin-top: 35px;">🧍‍♂️ Osobné údaje</h3>
-                    <p><strong>Meno:</strong> ${data.meno} ${data.priezvisko}</p>
-                    <p><strong>Email:</strong> ${data.email}</p>
-                    <p><strong>Telefón:</strong> ${data.telefon}</p>
-                    <p><strong>Rodné číslo:</strong> ${data.rodneCislo}</p>
-    
-                    <h3 style="margin-top: 35px;">🏠 Adresa</h3>
-                    <p><strong>Ulica:</strong> ${data.adresa}</p>
-                    <p><strong>Mesto:</strong> ${data.mesto}</p>
-                    <p><strong>PSČ:</strong> ${data.psc}</p>
-                    <p><strong>Krajina:</strong> ${data.krajina}</p>
-    
-                    <h3 style="margin-top: 35px;">📍 Miesto prevzatia a vrátenia</h3>
-                    <p><strong>Miesto prevzatia:</strong> ${data.m1}</p>
-                    <p><strong>Miesto vrátenia:</strong> ${data.m2}</p>
-    
-                    <h3 style="margin-top: 35px;">📅 Termín</h3>
-                    <p><strong>Od:</strong> ${formatDate(data.d1)} o ${data.c1}</p>
-                    <p><strong>Do:</strong> ${formatDate(data.d2)} o ${data.c2}</p>
-    
-                    <h3 style="margin-top: 35px;">💶 Platba</h3>
-                    <p><strong>Spôsob platby:</strong> ${data.platba === "cash" ? "V hotovosti" : "Bankovým prevodom"}</p>
-    
-                    <p style="margin-top: 40px; font-size: 0.9rem; color: #aaaaaa;">
-                        Tento e-mail bol vygenerovaný automaticky systémom <strong>FFcars</strong>.<br>
-                    </p>
-                    <p style="font-size: 0.7rem;"><strong>Neodpovedajte na tento e-mail</strong> – odpovede nie sú monitorované.</p>
+                    <img src="https://raw.githubusercontent.com/JakubKRATO/pozicovnaAut/main/static/img/auta/${data.auto.debugName}/1.webp" style="
+                        max-width: 300px;
+                        max-height: auto;
+                        border-radius: 5px;
+                        margin-top: 20px;">
+                    <h2>${data.auto.displayName}</h2>
+                        <h3 style="margin-top: 35px;">🧍‍♂️ Osobné údaje</h3>
+                        <p><strong>Meno:</strong> ${data.meno} ${data.priezvisko}</p>
+                        <p><strong>Email:</strong> ${data.email}</p>
+                        <p><strong>Telefón:</strong> ${data.telefon}</p>
+                        <p><strong>Rodné číslo:</strong> ${data.rodneCislo}</p>
+                        
+                        <h3 style="margin-top: 35px;">🏠 Adresa</h3>
+                        <p><strong>Ulica:</strong> ${data.adresa}</p>
+                        <p><strong>Mesto:</strong> ${data.mesto}</p>
+                        <p><strong>PSČ:</strong> ${data.psc}</p>
+                        <p><strong>Krajina:</strong> ${data.krajina}</p>
+                        
+                        <h3 style="margin-top: 35px;">📍 Miesto prevzatia a vrátenia</h3>
+                        <p><strong>Miesto prevzatia:</strong> ${data.m1}</p>
+                        <p><strong>Miesto vrátenia:</strong> ${data.m2}</p>
+                        
+                        <h3 style="margin-top: 35px;">📅 Termín</h3>
+                        <p><strong>Od:</strong> ${formatDate(data.d1)} o ${data.c1}</p>
+                        <p><strong>Do:</strong> ${formatDate(data.d2)} o ${data.c2}</p>
+                        
+                        <h3 style="margin-top: 35px;">💶 Platba</h3>
+                        <p><strong>Spôsob platby:</strong> ${data.platba === "cash" ? "V hotovosti" : "Bankovým prevodom"}</p>
+                        
+                        <p style="margin-top: 40px; font-size: 0.9rem; color: #aaaaaa;">
+                            Tento e-mail bol vygenerovaný automaticky systémom <strong>FFcars</strong>.<br>
+                        </p>
+                        <p style="font-size: 0.7rem;"><strong>Neodpovedajte na tento e-mail</strong> – odpovede nie sú monitorované.</p>
                 </div>`
     };
-        transport.sendMail(mailOptions, (err,info) => {
-            console.log(info,err);
-            
-            if (err) {
-                return err
-            }
-            return info
-        });
-        return true
+    transport.sendMail(mailOptions, (err,info) => {
+        console.log(info,err);
+        
+        if (err) {
+            return err
+        }
+        return info
+    });
+
+    return true
     } catch (error) {
         return false
     }
